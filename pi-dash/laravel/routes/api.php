4db05cf4c7c748ef0606d3d81b296df1da738996
Samuel Walz <walz.samuel@googlemail.com>
2018-07-15 00:04:11 +0200

    add cors, do request tests, eslint changes, fix migrate error

--- pi-dash/laravel/routes/api.php
+++ pi-dash/laravel/routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'cors'], function() {
+    Route::get('test', function () {
+        return response([1,2,3,5], 200);
+    });
+});
