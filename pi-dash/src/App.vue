1b0ffaaf11ff564372833edb14ff2ea52a4b9039
Samuel Walz <walz.samuel@googlemail.com>
2018-07-15 16:16:09 +0200

    change routes and indent formatting

--- pi-dash/src/App.vue
+++ pi-dash/src/App.vue
@@ -30,33 +30,3 @@
         color: #42b983;
     }
 </style>
-<script>
-export default {
-    created() {
-        const postData = {
-            client_id: 2,
-            client_secret: 'ShszeLwsH0LJUuCzKa49YzeO7rvtGe4S8DcOwMFL',
-            grant_type: 'password',
-            username: 'walz.samuel@gmail.com',
-            password: 'secret',
-            scope: '*'
-        };
-        this.$http.post('http://192.168.0.199:8000/oauth/token', postData)
-            .then(response => {
-            console.log(response);
-            const header = {
-                'Accept': 'application/json',
-                'Authorization': 'Bearer ' + response.body.access_token
-            };
-            this.$http.get('http://192.168.0.199:8000/api/user', {headers: header})
-                .then(response => {
-                    console.log(response);
-            }).catch(function(e) {
-                console.log(e); // "oh, no!"
-            });
-        }).catch(function(e) {
-            console.log(e); // "oh, no!"
-        });
-    }
-}
-</script>
--- pi-dash/src/main.js
+++ pi-dash/src/main.js
@@ -8,17 +8,17 @@ Vue.use(VueResource);
 
 Vue.component(
     'passport-clients',
-    require('./components/passport/Clients.vue')
+    require('./components/passport/Clients.vue'),
 );
 
 Vue.component(
     'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue')
+    require('./components/passport/AuthorizedClients.vue'),
 );
 
 Vue.component(
     'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue')
+    require('./components/passport/PersonalAccessTokens.vue'),
 );
 
 new Vue({
