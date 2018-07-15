9962b7b3d58fd32f53dbe9f4caaf73acbdf137ec
Samuel Walz <walz.samuel@googlemail.com>
2018-07-15 13:05:24 +0200

    add user seeder and missing cors config

---
+++ pi-dash/laravel/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+            'name' => 'admin',
+            'email' => 'walz.samuel@gmail.com',
+            'password' => Hash::make('secret'),
+        ];
+
+        User::create($admin);
+    }
+}
