a71569ed1d244cdfddfce77bdc95e28d1ae6c88c
Samuel Walz <walz.samuel@googlemail.com>
2018-07-20 23:56:21 +0200

    {SP] prepare commands for demonstration

--- pi-speech/mic_ps.c
+++ pi-speech/mic_ps.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <pocketsphinx.h>
 #include <sphinxbase/ad.h>
 #include <sphinxbase/err.h>
@@ -16,14 +19,15 @@ uint8 utt_started, in_speech;      // flags for tracking active speech - has spe
 int32 k;                           // holds the number of frames in the audio buffer
 char const *hyp;                   // pointer to "hypothesis" (best guess at the decoded result)
 char const *decoded_speech;
-
+bool is_tts_voice_female;
 
 int main(int argc, char *argv[])
 {
+
     config = cmd_ln_init(NULL, ps_args(), TRUE,                         // Load the configuration structure - ps_args() passes the default values
         "-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us",      // path to the standard english language model
-        "-lm", "models/old_pi_commands.lm",                                 // custom language model (file must be present)
-        "-dict", "models/old_pi_commands.dic",                              // custom dictionary (file must be present)
+        "-lm", "models/pi_commands.lm",                                 // custom language model (file must be present)
+        "-dict", "models/pi_commands.dic",                              // custom dictionary (file must be present)
         "-logfn", "/dev/null",                                          // suppress log info from being sent to screen
         NULL);
 
@@ -31,10 +35,56 @@ int main(int argc, char *argv[])
     // ad = ad_open_dev("plughw:1,0", (int) cmd_ln_float32_r(config, "-samprate")); // open default microphone at default samplerate
     ad = ad_open_dev("plughw:1,0", 16000);
 
+    tts_set_voice(VOICE_FEMALE1);
+    is_tts_voice_female = true;
+
     while(1) {
         decoded_speech = recognize_from_microphone();                    // call the function to capture and decode speech
-        printf("You Said: %s\n", decoded_speech);
-        tts_read(decoded_speech);
+        if (strcmp ("", decoded_speech) != 0) {
+            printf("You Said: %s\n", decoded_speech);
+
+            // string search    if(strstr(string, "WEATHER"))
+            // string compare   if(strcmp(string, "WEATHER"))
+
+            if (strstr(decoded_speech, "WEATHER")) {
+                tts_read("The weather in Karlsruhe is sonny with 26 degree Celsius");
+            }  else if (strstr(decoded_speech, "NEED HELP")) {
+                tts_read("I am sorry. .. I can not help you");
+            } else if (strstr(decoded_speech, "MUSIC")) {
+                tts_read("Now Playing .. Sleep Away .. from Bob Acri");
+                system("sudo mpg123 -n 313 /home/pi/music/sleep_away.mp3");
+            }  else if (strstr(decoded_speech, "TODAY")) {
+                tts_read("Today is the 19 July 2018");
+            }  else if (strstr(decoded_speech, "TOMORROW")) {
+                tts_read("Tomorrow is the 20 July 2018");
+            }  else if (strstr(decoded_speech, "SHUT DOWN")) {
+                if (is_tts_voice_female) {
+                    tts_read("System is shutting down now! in 3.. 2.. 1.. go............................ just a prank");
+                } else {
+                    tts_read("System is shutting down now. Not really!");
+                }
+            }  else if (strstr(decoded_speech, "WIKI")) {
+                tts_read("Wikipedia access is still under construction");
+            }  else if (strstr(decoded_speech, "CHANGE VOICE")) {
+                if (is_tts_voice_female) {
+                    tts_set_voice(VOICE_MALE1);
+                    is_tts_voice_female = false;
+                    tts_read("Hello everyone! I am Mr. Smith");
+
+                } else {
+                    tts_set_voice(VOICE_FEMALE1);
+                    is_tts_voice_female = true;
+                    tts_read("Good day, i am Tiffany");
+                }
+            }  else if (strstr(decoded_speech, "TIME")) {
+                time_t now;
+            	now = time(0);
+                tts_read(ctime(&now));
+            }
+//            else {
+//              tts_read(decoded_speech);
+//            }
+        }
 
     }
 
--- pi-speech/services/tts.c
+++ pi-speech/services/tts.c
@@ -1,10 +1,13 @@
 #include "tts.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+const char * list[] = {VOICE_FEMALE1,VOICE_FEMALE2,VOICE_MALE1,VOICE_MALE2};
 
 void tts_read (const char *text)
 {
-    char *cl_start = "echo 'You Said: ";
+    char *cl_start = "echo '";
     char *cl_end = "' | festival --tts";
     char str[255];
 
@@ -15,18 +18,36 @@ void tts_read (const char *text)
     system(str);
 }
 
-
-void tts_change_voice (const char *voice)
+void tts_read_with (const char *text_start, const char *text)
 {
-    if (voice == VOICE_MALE1 || voice == VOICE_MALE2 || voice == VOICE_FEMALE1 || voice == VOICE_FEMALE2) {
-        char *cl_start = "sudo echo '(set! voice_default \'";
-        char *cl_end = ")' >>  home/pi/.festivalrc";
-        char str[80];
+    char *cl_start = "echo '";
+    char *cl_end = "' | festival --tts";
+    char str[255];
 
-        strcpy(str, cl_start);
-        strcat(str, voice);
-        strcat(str, cl_end);
+    strcpy(str, cl_start);
+    strcat(str, text_start);
+    strcat(str, text);
+    strcat(str, cl_end);
 
-        system(str);
+    system(str);
+}
+
+
+void tts_set_voice (const char *voice)
+{
+    int i;
+    for(i=0; i<4; i++) {
+        if (strstr(list[i], voice) != NULL) {
+            char *cl_start = "sudo echo \"(set! voice_default \'";
+            char *cl_end = ")\" >  /home/pi/.festivalrc";
+            char str[80];
+
+            strcpy(str, cl_start);
+            strcat(str, voice);
+            strcat(str, cl_end);
+
+            system(str);
+            break;
+        }
     }
 }
